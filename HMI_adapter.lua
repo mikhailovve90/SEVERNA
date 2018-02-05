ad8e145d33d2fce8df957034f2e5d2b9f81f40af
Mikhailov Vladimir <mikhailov.ve90@gmail.com>
2018-02-05 13:59:11 +0300

    добавлены заземляющие ножы, тележки в приложение HMI_adapter

--- HMI_adapter.lua
+++ HMI_adapter.lua
@@ -22,9 +22,45 @@ signal_spac_name = { ["RAW_SPAC_L2210_RPV_DP"] = "switch.q.on",
                      ["RAW_SPAC_L2210_DEVICE1_STATUS"] = "switch.q.valid",
                      ["RAW_SPAC_L2210_SIG_BLOCK_VIHOD2_DP[2]"] = "switch.q.avoff"}
                      
+                     
+zn_prefix = {
+               ["GSP_SEV_KSSEV_E_USOE_DI_KRU1_1_QN_ON."] = "HMI_ZRU_OL1_1.",
+               ["GSP_SEV_KSSEV_E_USOE_DI_KRU1_2_QN_ON."] = "HMI_ZRU_OL1_2.",
+               ["GSP_SEV_KSSEV_E_USOE_DI_KRU1_3_QN_ON."] = "HMI_ZRU_OL1_3.",
+               ["GSP_SEV_KSSEV_E_USOE_DI_KRU1_4_QN_ON."] = "HMI_ZRU_OL1_4.",
+               ["GSP_SEV_KSSEV_E_USOE_DI_KRU1_6_QN_ON."] = "HMI_ZRU_OL1_6.",
+               ["GSP_SEV_KSSEV_E_USOE_DI_KRU1_7_QN_ON."] = "HMI_ZRU_OL1_7.",
+               ["GSP_SEV_KSSEV_E_USOE_DI_KRU1_8_QN_ON."] = "HMI_ZRU_OL1_8.",
+               ["GSP_SEV_KSSEV_E_USOE_DI_KRU1_12_QN_ON."] ="HMI_ZRU_OL1_12.",
+               ["GSP_SEV_KSSEV_E_USOE_DI_KRU2_6_QN_ON."] = "HMI_ZRU_OL2_6.",
+               ["GSP_SEV_KSSEV_E_USOE_DI_KRU2_8_QN_ON."] = "HMI_ZRU_OL2_8.",
+               ["GSP_SEV_KSSEV_E_USOE_DI_KRU2_9_QN_ON."] = "HMI_ZRU_OL2_9.",
+               ["GSP_SEV_KSSEV_E_USOE_DI_KRU2_10_QN_ON."] = "HMI_ZRU_OL2_10.",
+               ["GSP_SEV_KSSEV_E_USOE_DI_KRU2_11_QN_ON."] = "HMI_ZRU_OL2_11.",
+--             ["GSP_SEV_KSSEV_E_USOE_DI_KRU2_12_QN_ON."] = "HMI_ZRU_OL2_12.",
+               ["GSP_SEV_KSSEV_E_USOE_DI_KRU2_13_QN_ON."] = "HMI_ZRU_OL2_13.",
+               ["GSP_SEV_KSSEV_E_USOE_DI_KRU2_14_QN_ON."] = "HMI_ZRU_OL2_14.",
+               ["GSP_SEV_KSSEV_E_USOE_DI_KRU2_15_QN_ON."] = "HMI_ZRU_OL2_15.",
+               ["GSP_SEV_KSSEV_E_USOE_DI_KRU2_16_QN_ON."] = "HMI_ZRU_OL2_16.",
+               ["GSP_SEV_KSSEV_E_USOE_DI_KRU2_17_QN_ON."] = "HMI_ZRU_OL2_17.",
+               ["GSP_SEV_KSSEV_E_USOE_DI_KRU2_18_QN_ON."] = "HMI_ZRU_OL2_18."
+               }
 
--- адаптер который по изменению значения с устройства изменяет значение в ЧМИ 
-function adapter(param_tables)
+signal_zn_name = { ["Value"] = "zn.on",
+                   ["reliabilityFlag"] = "zn.valid"}
+
+truck_prefix = {
+                ["GSP_SEV_KSSEV_E_USOE_DI_KRU2_1_SQ_ON."] = "HMI_ZRU_SRZ."
+               }
+  
+signal_truck_name = {
+                      ["Value"] = "q.on",
+                      ["reliabilityFlag"] = "q.valid"}
+
+  
+                   
+-- адаптер который по изменению значения с устройства изменяет значение в ЧМИ для спака
+function adapter_spac(param_tables)
   spac_name = param_tables[1]
   hmi_spac_name = param_tables[2]
   spac_signal = param_tables[3]
@@ -41,22 +77,77 @@ function adapter(param_tables)
     Core[hmi_spac_name..hmi_spac_signal] = signal[1]
   end
 end
+-- адаптер который по изменению значения с устройства изменяет значение в ЧМИ для заземляющих ножей
+function adapter_zn(param_tables)
+  zn_name = param_tables[1]
+  hmi_zn_name = param_tables[2]
+  zn_signal = param_tables[3]
+  hmi_zn_signal = param_tables[4]
   
+  Core[hmi_zn_name..hmi_zn_signal] = Core[zn_name..zn_signal]
+  if zn_signal == "Value" then
+    if Core[zn_name..zn_signal] then 
+      Core[hmi_zn_name.."zn.off"] = false
+    else
+      Core[hmi_zn_name.."zn.off"] = true
+    end
+  end
+end
+-- адаптер который по изменению значения с устройства изменяет значение в ЧМИ для тележки 
+function adapter_truck(param_tables)
+  truck_name = param_tables[1]
+  hmi_truck_name = param_tables[2]
+  truck_signal = param_tables[3]
+  hmi_truck_signal = param_tables[4]
+  
+  Core[hmi_truck_name..hmi_truck_signal] = Core[truck_name..truck_signal]
+  if truck_signal == "Value" then
+    if Core[truck_name..truck_signal] then 
+      Core[hmi_truck_name.."q.off"] = false
+    else
+      Core[hmi_truck_name.."q.off"] = true
+    end
+  end
+end
 
 -- инициализация  
 for spac_name, hmi_spac_name in pairs(spac_prefix) do
   for spac_signal, hmi_spac_signal in pairs(signal_spac_name) do 
-    Core.addLogMsg(spac_name..spac_signal)  
-    adapter({spac_name, hmi_spac_name, spac_signal, hmi_spac_signal})
+    adapter_spac({spac_name, hmi_spac_name, spac_signal, hmi_spac_signal})
   end
 end
 
 -- сторож по изменению
 for spac_name, hmi_spac_name in pairs(spac_prefix) do
   for spac_signal, hmi_spac_signal in pairs(signal_spac_name) do
-    Core.addLogMsg(spac_name..spac_signal)
-    Core.onExtChange({spac_name..spac_signal}, adapter, {spac_name, hmi_spac_name, spac_signal, hmi_spac_signal})
+    Core.onExtChange({spac_name..spac_signal}, adapter_spac, {spac_name, hmi_spac_name, spac_signal, hmi_spac_signal})
   end
 end
 
+-- инициализация  
+for zn_name, hmi_zn_name in pairs(zn_prefix) do
+  for zn_signal, hmi_zn_signal in pairs(signal_zn_name) do 
+    adapter_zn({zn_name, hmi_zn_name, zn_signal, hmi_zn_signal})
+  end
+end
+
+-- сторож по изменению
+for zn_name, hmi_zn_name in pairs(zn_prefix) do
+  for zn_signal, hmi_zn_signal in pairs(signal_zn_name) do
+    Core.onExtChange({zn_name..zn_signal}, adapter_zn, {zn_name, hmi_zn_name, zn_signal, hmi_zn_signal})
+  end
+end
+
+for truck_name, hmi_truck_name in pairs(truck_prefix) do
+  for truck_signal, hmi_truck_signal in pairs(signal_truck_name) do 
+    adapter_truck({truck_name, hmi_truck_name, truck_signal, hmi_truck_signal})
+  end
+end
+
+-- сторож по изменению
+for truck_name, hmi_truck_name in pairs(truck_prefix) do
+  for truck_signal, hmi_truck_signal in pairs(signal_truck_name) do
+    Core.onExtChange({truck_name..truck_signal}, adapter_truck, {truck_name, hmi_truck_name, truck_signal, hmi_truck_signal})
+  end
+end
 Core.waitEvents()
